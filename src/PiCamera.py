ab40b3240d3af51876e60a87c29eeee794d27c5a
Sohaib Alam <sacreative10@gmail.com>
2023-12-29 15:29:42 +0000

    Fixed PiCamera.py

--- src/PiCamera.py
+++ src/PiCamera.py
@@ -5,18 +5,19 @@ from camera import Camera
 
 class PiCamera(Camera):
     @staticmethod
+    def frames():
+        with picamera.PiCamera() as camera:
+            # let camera warm up
+            time.sleep(2)
 
-    with picamera.PiCamera() as camera:
-        # let camera warm up
-        time.sleep(2)
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg',
+                                               use_video_port=True):
+                # return current frame
 
-        stream = io.BytesIO()
-        for _ in camera.capture_continuous(stream, 'jpeg',
-                                           use_video_port=True):
-            # return current frame
-            stream.seek(0)
-            yield stream.read()
+                stream.seek(0)
+                yield stream.read()
 
-            # reset stream for next frame
-            stream.seek(0)
-            stream.truncate()
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
